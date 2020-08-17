d6e0255319b0052020d2b098d70f3b6b2595585e
qpthedev <qpthedev@gmail.com>
2020-08-17 07:15:51 -0400

    Define routes

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Detail from "../Routes/Detail";
+import Home from "../Routes/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/:id" component={Detail}></Route>
+      </Router>
+    </>
+  );
 }
 
 export default App;
