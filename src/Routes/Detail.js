fca5f77d02b0a645527ff609b2b5dc5991f54303
qpthedev <qpthedev@gmail.com>
2020-08-17 07:16:24 -0400

    Page view

---
+++ src/Routes/Detail.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+export default () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: parseInt(id) },
+  });
+  if (loading) {
+    return "Loading...";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
+};
